819b441dfbd53b69edf566937c340787740e1a13
Riza <rzakaan@gmail.com>
2022-01-05 19:29:30 +0300

    add configuration and setup file for pypi

--- Makefile
+++ Makefile
@@ -1,6 +1,21 @@
 PROJECT="sockxml"
+DIST="dist"
+BUILD="build"
 
 test:
 	python -m unittest sockxml __test__/main_test.py -v
-dist:
-	python setup.py sdist
+
+clean:
+	rm -rf $DIST
+	rm -rf $BUILD
+	rm -rf __pycache__
+	rm -rf *.pyc
+	rm -rf *.egg
+	rm -rf *.egg-info
+
+build:
+	python setup.py sdist bdist_wheel
+	twine check dist/* && twine upload dist/*
+
+publish:
+	python -m twine upload --repository testpypi dist/*
--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
-    name = 'sockxml',
-    packages = ['sockxml'],
     version = '0.0.1',
+    name = 'sockxml',
+    packages = [],
     description = 'XML Reader for Socket Code Generator',
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/rzakaan/sockxml',
     author = 'Riza Kaan Ucak',
     author_email = 'rzakaan@gmail.com',
     license = 'MIT',
-    url = 'https://github.com/rzakaan/sockxml',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
     install_requires = [],
     download_url = 'https://github.com/rzakaan/sockxml/archive/0.0.1.tar.gz',
     keywords = ['xml', 'socket', 'code generator'],
-    classifiers = [],
     python_requires='>=3',
 )
